dbc10419567cced8fb18d231feecb2f8177a31d1
Roobinisubramanian <165560042+Roobinisubramanian@users.noreply.github.com>
2024-05-20 08:43:17 +0530

    Dom tree

---
+++ script.js
@@ -0,0 +1,15 @@
+function main1(){
+var Firstname = document.getElementById("Firstname").value;
+var Middlename = document.getElementById("Middlename").value;
+var Lastname = document.getElementById("Lastname").value;
+var Email = document.getElementById("Email").value;
+var Phonenumber = document.getElementById("Phonenumber").value;
+
+console.log(Firstname);
+console.log(Middlename);
+console.log(Lastname);
+console.log(Email);
+console.log(Phonenumber);
+
+}
+ 
